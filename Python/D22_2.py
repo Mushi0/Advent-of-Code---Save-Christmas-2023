4fffeff9c38ca467741c6814d9cc4fea1afd7658
Mushi0 <zhy_980401@outlook.com>
2024-01-12 19:13:13 +0000

    I didn't need to sort

--- Python/D22_2.py
+++ Python/D22_2.py
@@ -32,9 +32,8 @@ def main(DATA_INPUT):
                 for yy in range(y_1, y_2 + 1):
                     if height_map[xx][yy] > max_height:
                         max_height = height_map[xx][yy]
-            heapq.heappush(settled_bricks, (max_height + 1, 
-                                        (brick[1][0], brick[1][1], max_height + 1), 
-                                        (brick[2][0], brick[2][1], max_height + 1)))
+            settled_bricks.append(((brick[1][0], brick[1][1], max_height + 1), 
+                                    (brick[2][0], brick[2][1], max_height + 1)))
             for xx in range(x_1, x_2 + 1):
                 for yy in range(y_1, y_2 + 1):
                     height_map[xx][yy] = max_height + 1
@@ -43,36 +42,29 @@ def main(DATA_INPUT):
             y = brick[1][1]
             brick_len = abs(brick[1][2] - brick[2][2]) + 1
             max_height = height_map[x][y]
-            heapq.heappush(settled_bricks, (max_height + 1, 
-                                            (x, y, max_height + 1), 
-                                            (x, y, max_height + brick_len)))
+            settled_bricks.append(((x, y, max_height + 1), 
+                                (x, y, max_height + brick_len)))
             height_map[x][y] = max_height + brick_len
-    
-    # sort the bricks by height
-    new_settled_bricks = []
-    while settled_bricks:
-        new_settled_bricks.append(heapq.heappop(settled_bricks))
-    settled_bricks = new_settled_bricks
     
     # get the top and bottum bricks
     new_settled_bricks = []
     for i, brick in enumerate(settled_bricks):
-        x_1 = min(brick[1][0], brick[2][0])
-        x_2 = max(brick[1][0], brick[2][0])
-        y_1 = min(brick[1][1], brick[2][1])
-        y_2 = max(brick[1][1], brick[2][1])
-        z_1 = max(brick[1][2], brick[2][2])
-        z_2 = min(brick[1][2], brick[2][2])
+        x_1 = min(brick[0][0], brick[1][0])
+        x_2 = max(brick[0][0], brick[1][0])
+        y_1 = min(brick[0][1], brick[1][1])
+        y_2 = max(brick[0][1], brick[1][1])
+        z_1 = max(brick[0][2], brick[1][2])
+        z_2 = min(brick[0][2], brick[1][2])
 
         top = []
         bottum = []
         for j, brick_1 in enumerate(settled_bricks):
-            x_1_1 = min(brick_1[1][0], brick_1[2][0])
-            x_1_2 = max(brick_1[1][0], brick_1[2][0])
-            y_1_1 = min(brick_1[1][1], brick_1[2][1])
-            y_1_2 = max(brick_1[1][1], brick_1[2][1])
-            z_1_2 = max(brick_1[1][2], brick_1[2][2])
-            z_2_2 = min(brick_1[1][2], brick_1[2][2])
+            x_1_1 = min(brick_1[0][0], brick_1[1][0])
+            x_1_2 = max(brick_1[0][0], brick_1[1][0])
+            y_1_1 = min(brick_1[0][1], brick_1[1][1])
+            y_1_2 = max(brick_1[0][1], brick_1[1][1])
+            z_1_2 = max(brick_1[0][2], brick_1[1][2])
+            z_2_2 = min(brick_1[0][2], brick_1[1][2])
             if ((x_1 <= x_1_2 and x_2 >= x_1_1) and 
                  (y_1 <= y_1_2 and y_2 >= y_1_1)):
                 if z_1 + 1 == z_2_2 and i != j:
@@ -80,29 +72,26 @@ def main(DATA_INPUT):
                 elif z_2 == z_1_2 + 1 and i != j:
                     bottum.append(j)
         
-        new_settled_bricks.append((brick[0], brick[1], brick[2], tuple(top), tuple(bottum)))
+        new_settled_bricks.append((brick[0], brick[1], tuple(top), tuple(bottum)))
     
     # update the top and bottum bricks while one brick is removed
     def fall(brick_id):
         global new_settled_bricks_copy
 
-        top = new_settled_bricks_copy[brick_id][3]
+        top = new_settled_bricks_copy[brick_id][2]
         for brick_1_id in top:
             brick_1 = new_settled_bricks_copy[brick_1_id]
             new_settled_bricks_copy[brick_1_id] = (brick_1[0], 
                                             brick_1[1], 
                                             brick_1[2], 
-                                            brick_1[3], 
-                                            tuple([i for i in brick_1[4] if i != brick_id]))
-            if len(brick_1[4]) == 1:
-                if not len(new_settled_bricks_copy[brick_1_id][4]) == 0:
-                    print(f'wrong: {brick_id}, {new_settled_bricks_copy[brick_id]}, {brick_1_id}, {brick_1}, {new_settled_bricks_copy[brick_1_id]}')
+                                            tuple([i for i in brick_1[3] if i != brick_id]))
+            if len(brick_1[3]) == 1:
                 fall(brick_1_id)
 
     # count the number of bricks that are on the top for calculation
     top_bircks = 0
     for i, (brick) in enumerate(new_settled_bricks):
-        if len(brick[4]) == 0:
+        if len(brick[3]) == 0:
             top_bircks += 1
 
     nb_fall = 0
@@ -110,7 +99,7 @@ def main(DATA_INPUT):
     for i in range(len(new_settled_bricks)):
         new_settled_bricks_copy = new_settled_bricks.copy()
         fall(i)
-        nb_fall += sum([1 for brick in new_settled_bricks_copy if len(brick[4]) == 0]) - top_bircks
+        nb_fall += sum([1 for brick in new_settled_bricks_copy if len(brick[3]) == 0]) - top_bircks
             
     print(f'Time taken: {(time.time() - start_time):.3e}s')
     print(f'The sum of the number of other bricks that would fall is: {nb_fall}')
